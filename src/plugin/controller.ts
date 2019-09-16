1f411c5a78d026ec399c4081258973d2718bb4e9
Daniel Destefanis <destefanis.drdestefanis@gmail.com>
2019-09-15 22:32:53 -0700

    Updated controller with comments

--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -1,5 +1,7 @@
 figma.showUI(__html__);
 
+// Recursive function to assign the current node
+// to a new object and check if it has children
 function checkIfNested(node) {
   let copyNode = new Object();
   copyNode.name = node.name;
@@ -20,6 +22,7 @@ function checkIfNested(node) {
 }
 
 figma.ui.onmessage = msg => {
+  // Fetch a specific node by ID.
   if (msg.type === "fetch-layer-data") {
     let layer = figma.getNodeById(msg.id);
     let returnData = checkIfNested(layer);
@@ -30,7 +33,9 @@ figma.ui.onmessage = msg => {
     });
   }
 
+  // Initalize the app
   if (msg.type === "run-app") {
+    // Traverses the node tree
     function traverse(node) {
       if ("children" in node) {
         if (node.type !== "INSTANCE") {
@@ -42,21 +47,25 @@ figma.ui.onmessage = msg => {
       return node;
     }
 
+    // Loop through the current selection in Figma.
     let allNodes = traverse(figma.currentPage.selection);
     let allResults = [];
 
+    // Loop through each node and a copy of that object to the array.
     allNodes.forEach(function(node) {
       let copyNode = new Object();
       copyNode = checkIfNested(node);
       allResults.push(copyNode);
     });
 
+    // Pass the array back to the UI to be displayed.
     figma.ui.postMessage({
       type: "complete",
       message: allResults
     });
   }
 
+  // To be used for fetching style dev info.
   function checkForStyle(node) {
     if (node.fillStyleId) {
       let style = figma.getStyleById(node.fillStyleId);
